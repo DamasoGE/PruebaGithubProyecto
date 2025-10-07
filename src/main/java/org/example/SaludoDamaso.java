001a4f08515c52c5e3ee549a32ea37dc8a1b4f87
DamasoGE <franciscodamasogimenez@gmail.com>
2025-10-07 20:27:50 +0200

    Commit Damaso

---
+++ src/main/java/org/example/SaludoDamaso.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class SaludoDamaso {
+
+    public static void main(String[] args) {
+        decirHola();
+    }
+
+    public static void decirHola() {
+        System.out.println("Hola, soy Damaso");
+    }
+}
